0a735778a234d56fa613f6881dde0bb98cea4beb
Jason <git-admin@casjaysdev.com>
2022-01-10 22:14:40 -0500

    🗃 Modified: install.sh  🗃

--- install.sh
+++ install.sh
@@ -133,26 +133,27 @@ fi
 # run post install scripts
 run_postinst() {
   systemmgr_run_post
-  if [ ! -L /boot/grub/themes/default ] && cmd_exists grub-mkconfig && [ -f /boot/grub/grub.cfg ] && [ -f /etc/default/grub ]; then
+  [ -f "$INSTDIR/.inst" ] && return
+  GRUB="$(builtin type -P grub2-mkconfig 2>/dev/null || builtin type -P grub-mkconfig 2>/dev/null || echo '')"
+  if [[ -f "$GRUB" ]] && [ -f /boot/grub/grub.cfg ] && [ -f /etc/default/grub ]; then
     GRUB="/usr/sbin/grub-mkconfig"
     mkd /boot/grub/themes
     cp_rf /etc/default/grub /etc/default/grub.bak
     cp_rf "$APPDIR/themes/." /boot/grub/themes/
     cp_rf "$APPDIR/grub" /etc/default/grub
-    ln_sf /boot/grub/themes/poly-dark /boot/grub/themes/default
     sed -i 's|^\(GRUB_TERMINAL\w*=.*\)|#\1|' /etc/default/grub
     sed -i 's|grubdir|grub|g' /etc/default/grub
+    [ -L /boot/grub/themes/default ] || ln_sf "/boot/grub/themes/poly-dark" "/boot/grub/themes/default"
     ${GRUB} -o /boot/grub/grub.cfg
 
-  elif [ ! -L /boot/grub2/themes/default ] && { cmd_exists grub2-mkconfig || cmd_exists grub-mkconfig; } && [ -f /boot/grub2/grub.cfg ] && [ -f /etc/default/grub ]; then
-    GRUB="$(builtin type -P /usr/sbin/grub2-mkconfig 2>/dev/null || builtin type -P /usr/sbin/grub-mkconfig)"
+  elif [[ -f "$GRUB" ]] && [ -f /boot/grub2/grub.cfg ] && [ -f /etc/default/grub ]; then
     mkd /boot/grub2/themes
     cp_rf /etc/default/grub /etc/default/grub.bak
     cp_rf "$APPDIR/themes/." /boot/grub2/themes/
     cp_rf "$APPDIR/grub" /etc/default/grub
-    ln_sf /boot/grub2/themes/poly-dark /boot/grub2/themes/default
     sed -i 's|^\(GRUB_TERMINAL\w*=.*\)|#\1|' /etc/default/grub
     sed -i 's|grubdir|grub2|g' /etc/default/grub
+    [ -L /boot/grub2/themes/default ] || ln_sf "/boot/grub2/themes/poly-dark" "/boot/grub2/themes/default"
     ${GRUB} -o /boot/grub2/grub.cfg
   fi
   touch "$INSTDIR/.inst"
