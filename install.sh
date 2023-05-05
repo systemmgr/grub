21259998c00fd633178a4dda205dac00a7099998
casjay <git-admin@casjaysdev.com>
2023-05-05 14:22:10 -0400

    🗃️ Committing everything that changed 🗃️
    install.sh

--- install.sh
+++ install.sh
@@ -134,22 +134,20 @@ fi
 # run post install scripts
 run_postinst() {
   systemmgr_run_post
-  GRUB="$(builtin type -P grub2-mkconfig 2>/dev/null || builtin type -P grub-mkconfig 2>/dev/null || echo '')"
-  GRUB_CONF="$(find /boot -type f -name 'grub.cfg' 2>/dev/null | grep '^')"
-  GRUB_HOME="$(dirname "$GRUB_CONF" 2>/dev/null | grep '^')"
-  DISTRO_NAME=$(grep -sE --no-filename '^NAME=|^DISTRIB_ID=|^ID=' /etc/*-release | head -n1 | sed 's|.*=||g' | grep '^' || echo "Linux")
-  if [ -f "$GRUB" ] && [ -f "$GRUB_CONF" ] && [ -f "/etc/default/grub" ]; then
+  GRUB="$(builtin type -P grub2-mkconfig 2>/dev/null || builtin type -P grub-mkconfig 2>/dev/null || false)"
+  GRUB_CONF="$(find /boot -type f -iname '*grub*' 2>/dev/null | grep -F '.cfg' | grep '^' || false)"
+  GRUB_HOME="$([ -n "$GRUB_CONF" ] && dirname "$GRUB_CONF" 2>/dev/null | grep '^' || false)"
+  DISTRO_NAME="$(grep -sE --no-filename '^NAME=|^DISTRIB_ID=|^ID=' /etc/*-release | head -n1 | sed 's|.*=||g;s|"||g' | grep '^' || echo "Linux")"
+  if [ -n "$GRUB" ] && [ -n "$GRUB_CONF" ] && [ -f "/etc/default/grub" ]; then
     mkd "$GRUB_HOME/themes"
-    if [ -n "$FORCE_INSTALL" ] && [ ! -f "$INSTDIR/.installed" ]; then
-      cp_rf "/etc/default/grub" "/etc/default/grub.bak"
+    if [ -n "$FORCE_INSTALL" ] || [ ! -f "$INSTDIR/.installed" ]; then
+      cp_rf "/etc/default/grub" "$APPDIR/grub.bak"
       cp_rf "$APPDIR/themes/." "$GRUB_HOME/themes/"
       cp_rf "$APPDIR/grub" "/etc/default/grub"
       sed -i 's|grubdir|grub|g' "/etc/default/grub"
     fi
     sed -i 's|^\(GRUB_TERMINAL\w*=.*\)|#\1|' "/etc/default/grub"
     sed -i "s|^GRUB_DISTRIBUTOR=.*|GRUB_DISTRIBUTOR=\"$DISTRO_NAME\"|g" "/etc/default/grub"
-    sed -i '1,/GRUB_DISTRIBUTOR/ s/""/"/;s|""$|"|g' "/etc/default/grub"
-    sed -i -e '/^GRUB_DISTRIBUTOR/s/""/"/g' "/etc/default/grub"
     [ -L "$GRUB_HOME/themes/default" ] || ln_sf "$GRUB_HOME/themes/poly-dark" "$GRUB_HOME/themes/default"
     ${GRUB} -o "$GRUB_HOME/grub.cfg"
   fi
